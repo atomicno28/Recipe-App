9a3321c8bf168d1bf5c13bbcbb1515711d95ca52
Nikhil Patel <nikhilid2205@gmail.com>
2023-05-14 15:35:19 +0530

    setting the schema for recipee

---
+++ server/src/models/Recipes.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const RecipeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  ingredients: [{ type: String, required: true }],
+  instructions: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  cookingTime: { type: Number, require: true },
+
+  // one who created the dish.
+  // ref: it denotes the schema of users
+  userOwner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+});
+
+export const RecipeModel = mongoose.model("recipes", RecipeSchema);
