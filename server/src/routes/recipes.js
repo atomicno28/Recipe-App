854956cdd6d78b5e0620efb6ad1834ec3f8b13c3
Nikhil Patel <nikhilid2205@gmail.com>
2023-05-17 22:59:22 +0530

    backend stuffs.

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
@@ -20,13 +21,49 @@ router.post("/", async (req, res) => {
   // if(!user) res.json({message:"Dish already present."})
   // const newUser = new RecipeModel({name,ingredients,instructions,imageUrl,cookingTime});
   // await newUser.save();
-  // res.json({message:"Dish added."})
+  // res.json({m essage:"Dish added."})
 
   // this is just an alternative.
   const recipe = new RecipeModel(req.body);
   try {
     const response = await recipe.save();
     res.json(response);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const recipe = await RecipeModel.findById(req.body.recipeID);
+    const user = await UserModel.findById(req.body.userID);
+
+    // add something to the user with that ID.
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.json({ savedRecipes: user.savedRecipes });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.get("/savedRecipes/ids", async (req, res) => {
+  try {
+    const user = await UserModel.findByiId(req.body.userID);
+    res.json({ savedRecipes: user.sacedRecipes });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.get("/savedRecipes/", async (req, res) => {
+  try {
+    const user = await UserModel.findByiId(req.body.userID);
+    const savedRecipes = await RecipeModel.find({
+      // id is in user's recipes.
+      _id: { $in: user.savedRecipes },
+    });
+    res.json({ savedRecipes });
   } catch (err) {
     res.json(err);
   }
